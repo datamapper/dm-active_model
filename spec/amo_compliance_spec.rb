4a6f6342e16a57dc12ea727e853f0d63c162c377
snusnu <gamsnjaga@gmail.com>
2010-01-20 18:04:23 +0100

    Updated active_model/lint delegation. All tests pass.

--- spec/amo_compliance_spec.rb
+++ spec/amo_compliance_spec.rb
@@ -1,6 +1,14 @@
 require 'spec/test/unit'
 
+# This must be kept in sync with active_model/lint tests
+# at least for as long as
+#
+#   http://rspec.lighthouseapp.com/projects/5645/tickets/900
+#
+# isn't resolved in some way (probably with rspec2)
+
 share_examples_for 'an active_model compliant object' do
+
   include ActiveModel::Lint::Tests
 
   it 'must implement the #valid? interface' do
@@ -15,12 +23,13 @@ share_examples_for 'an active_model compliant object' do
     test_destroyed?
   end
 
-  it 'must implement the #destroyed? interface' do
-    test_destroyed?
+  it 'must implement the #model_naming interface' do
+    test_model_naming
   end
 
   it 'must implement the #errors interface' do
     test_errors_aref
     test_errors_full_messages
   end
+
 end
