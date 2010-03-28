a9ac0984306c067d2ff77a19aa27060b0c74c870
snusnu <gamsnjaga@gmail.com>
2010-03-29 00:31:35 +0200

    Support running 'bundle exec rake spec'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,3 @@
-begin
-  # Just in case the bundle was locked
-  # This shouldn't happen in a dev environment but lets be safe
-  require File.expand_path('../../.bundle/environment', __FILE__)
-rescue LoadError
-  require 'rubygems'
-  require 'bundler'
-  Bundler.setup
-end
-Bundler.require(:default, :test)
-
 # Use local dm-core if running from a typical dev checkout.
 lib = File.expand_path('../../../dm-core/lib', __FILE__)
 $LOAD_PATH.unshift(lib) if File.directory?(lib)
